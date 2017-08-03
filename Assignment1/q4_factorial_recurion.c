a685c8851d8e436883f44723bc13291d85b3e26d
Subhajitdas298 <subhajitdas298@live.com>
2017-08-03 20:12:10 +0530

    Finished assignment 1
    Added Q4 & Q5

---
+++ Assignment1/q4_factorial_recurion.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int factorial(int num);
+
+int main() {
+    int n,fact;
+
+    printf("Enter no : ");
+    scanf("%d",&n);
+
+    fact = factorial(n);
+
+    printf("Factorial(%d) = %d",n,fact);
+
+    return 0;
+}
+
+/*
+Factorial is generated as,
+number * (number-1)
+stop when number equals 1
+*/
+int factorial(int num) {
+    if(num<=1) return 1;
+    return num * factorial(num-1);
+}
---
+++ Assignment1/q5_subtract.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int a,b;
+
+    printf("Enter two nos : ");
+    scanf("%d %d",&a,&b);
+
+    printf("%d - %d = %d",a,b,a-b);
+
+    return 0;
+}
